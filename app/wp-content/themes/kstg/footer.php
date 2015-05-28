0ef66e604873f7cfb3e7a0e2ff8d2cd6944da393
sarahlshowers <sarahlshowers@gmail.com>
2015-05-27 20:55:18 -0700

    Footer built for desktop and mobile TODO: Need to reflow: social media breaks at 1235px may want to consider a gird to help with easy reflowing

--- app/wp-content/themes/kstg/footer.php
+++ app/wp-content/themes/kstg/footer.php
@@ -1,38 +1,47 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-
-				<div id="inner-footer" class="wrap cf">
-          <ul class="navigation">
-            <li>
-              <p>© 2015 Kevin Smith Transportation Group</p>
+        <div class="copyright">
+          <p>© 2015 Kevin Smith Transportation Group</p>
+        </div>
+        <nav>
+         <ul class="nav">
+           <li>
+            <a href=""><p>Legal</p></a>
+           </li>
+           <li>
+             <a href=""><p>Lorem Ipsum</p></a>
+           </li>
+           <li>
+             <a href=""><p>Lorem Ipsum</p></a>
+           </li>
+         </ul>
+       </nav>
+       <div class="social">
+         <ul>
+           <li>
+              <a href="https://www.facebook.com/kevinsmithgroup" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-facebook.png" alt="Facebook" /></a>
             </li>
             <li>
-              <p>Legal</p>
+              <a href="https://www.twitter.com/Kevinsmithgrp" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-twitter.png" alt="Twitter" /></a>
             </li>
             <li>
-              <p>Lorem Ipsum</p>
+              <a href="https://www.linkedin.com/company/kevin-smith-transportation-group" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-linkedin.png" alt="LinkedIn" /></a>
             </li>
             <li>
-              <p>Lorem Ipsum</p>
+              <a href="https://www.youtube.com/channel/UCMeEufGeiAWaxOsIS0Lwegg" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-youtube.png" alt="YouTube" /></a>
             </li>
-          </ul>
+            <li>
+              <a href="https://plus.google.com/b/115066308991397006542/115066308991397006542/posts" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-gplus.png" alt="Google Plus" /></a>
+            </li>
+         </ul>
+       </div>
 
-          <ul class="social">
-            <li><a href="https://www.facebook.com/kevinsmithgroup" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-facebook.png" alt="Facebook" /></a></li>
-            <li><a href="https://www.twitter.com/Kevinsmithgrp" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-twitter.png" alt="Twitter" /></a></li>
-            <li><a href="https://www.linkedin.com/company/kevin-smith-transportation-group" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-linkedin.png" alt="LinkedIn" /></a></li>
-            <li><a href="https://www.youtube.com/channel/UCMeEufGeiAWaxOsIS0Lwegg" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-youtube.png" alt="YouTube" /></a></li>
-            <li><a href="https://plus.google.com/b/115066308991397006542/115066308991397006542/posts" target="_blank"><img src="<?php bloginfo('template_url'); ?>/library/images/icon-gplus.png" alt="Google Plus" /></a></li>
-          </ul>
+     </footer>
 
-				</div>
+   </div>
 
-			</footer>
+   <?php // all js scripts are loaded in library/bones.php ?>
+   <?php wp_footer(); ?>
 
-		</div>
+ </body>
 
-		<?php // all js scripts are loaded in library/bones.php ?>
-		<?php wp_footer(); ?>
-
-	</body>
-
-</html> <!-- end of site. what a ride! -->
+ </html> <!-- end of site. what a ride! -->
