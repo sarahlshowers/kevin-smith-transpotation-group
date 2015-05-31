4780ac98ac0be563e31450710c71d054bbd1546d
sarahlshowers <sarahlshowers@gmail.com>
2015-05-31 15:34:57 -0700

    initial markup

--- app/wp-content/themes/kstg/index.php
+++ app/wp-content/themes/kstg/index.php
@@ -1,76 +1,50 @@
 <?php get_header(); ?>
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline entry-meta vcard">
-                                                                        <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                       								/* the time the post was published */
-                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
-
-								</header>
-
-								<section class="entry-content cf">
-									<?php the_content(); ?>
-								</section>
-
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-									</p>
-
-
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php bones_page_navi(); ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-
-						</main>
-
-					<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
-
+<section class="main">
+	<div class="gallery-container">
+		<div class="gallery-corporate">
+			<img src="" alt="corporate services" />
+			<a href=""><p>COPORATE</p><img src="" /></a>
+		</div>
+		<div class="gallery-events">
+			<img src="" alt="personal services and events"/>
+			<a href=""><p>PERSONAL & EVENTS</p><img src="" /></a>
+		</div>
+		<div class="gallery-travel">
+			<img src="" alt="travel services"/>
+			<a href=""><p>TRAVEL</p><img src="" /></a>
+		</div>
+		<div class="gallery-wedding">
+			<img src="" alt="wedding services"/>
+			<a href=""><p>WEDDING</p><img src="" /></a>
+		</div>
+	</div>
+	<div class="about">
+		<h1>The Philadelphia Region's Newest Luxury Transportation Company</h1>
+		<p>Kevin Smith Transportation Group is the Philadelphia region's newest luxury limousine and total transportation service. We hope to change the way you think about limousine, airport pickup, and towncar service by providing you with a customized experience you'll love.</p>
+		<p>A personal account manager and a hand-picked chauffeur will ensure you have a smooth journey with KSTG. From your wedding or prom to your airport pickup, you count on us for reliability, luxury, and professionalism. We do the little things that matter to you, because they matter to us. Service, different.</p>
+		<div class="button">
+			<a href=""><p>MAKE A RESERVATION</p><img src="" /></a>
+		</div>
+	</div>
+</section>
+<aside>
+	<div class="newsletter">
+		<h2>SIGN UP FOR OUR NEWSLETTER</h2>
+		<form action="" id="newsletter">
+			<label>Email Address</label><br />
+			<input type="email" name="email"><br />
+		</form>
+		<button type="submit" form_id="newsletter" value="submit">SUBMIT<img src="" /></button>
+	</div>
+	<div class="contact-us">
+		<h2>CONTACT US</h2>
+		<h3>Kevin Smith Transportation Group</h3>
+		<p>123 Main Street</p>
+		<p>Anywhere, PA 12345</p>
+		<p><span class="phone">844.LIMO.KSG</span> (844.546.6574)</p>
+		<p>email@emailaddress.com</p>
+	</div>
+</aside>
 
 <?php get_footer(); ?>
