8db8ce8351e302bc21d77ccb0680f7787d01bdce
sarahlshowers <sarahlshowers@gmail.com>
2015-06-01 22:50:45 -0700

    home spage styling TODO: Add wrappers for max-width content TODO: mobile!!! TODO: Investigate a good responsive grid to leverage

--- app/wp-content/themes/kstg/index.php
+++ app/wp-content/themes/kstg/index.php
@@ -4,19 +4,19 @@
 	<div class="gallery-container">
 		<div class="gallery-corporate">
 			<img src="<?php bloginfo('template_url'); ?>/library/images/corporate.png" alt="corporate services" width="280" height="350" />
-			<a href=""><p>CORPORATE</p><img src="" /></a>
+			<a href=""><p>CORPORATE &#8594;</p><img src="" /></a>
 		</div>
 		<div class="gallery-events">
 			<img src="<?php bloginfo('template_url'); ?>/library/images/personal.png" alt="personal services and events" width="280" height="350" />
-			<a href=""><p>PERSONAL & EVENTS</p><img src="" /></a>
+			<a href=""><p>PERSONAL & EVENTS &#8594;</p><img src="" /></a>
 		</div>
 		<div class="gallery-travel">
 			<img src="<?php bloginfo('template_url'); ?>/library/images/travel.png" alt="travel services" width="280" height="350" />
-			<a href=""><p>TRAVEL</p><img src="" /></a>
+			<a href=""><p>TRAVEL &#8594;</p><img src="" /></a>
 		</div>
 		<div class="gallery-wedding">
 			<img src="<?php bloginfo('template_url'); ?>/library/images/wedding.png" alt="personal services and events" alt="wedding services" width="280" height="350" />
-			<a href=""><p>WEDDING</p><img src="" /></a>
+			<a href=""><p>WEDDING &#8594;</p><img src="" /></a>
 		</div>
 	</div>
 	<div class="about">
@@ -24,7 +24,7 @@
 		<p>Kevin Smith Transportation Group is the Philadelphia region's newest luxury limousine and total transportation service. We hope to change the way you think about limousine, airport pickup, and towncar service by providing you with a customized experience you'll love.</p>
 		<p>A personal account manager and a hand-picked chauffeur will ensure you have a smooth journey with KSTG. From your wedding or prom to your airport pickup, you count on us for reliability, luxury, and professionalism. We do the little things that matter to you, because they matter to us. Service, different.</p>
 		<div class="button">
-			<a href=""><p>MAKE A RESERVATION</p><img src="" /></a>
+			<a href=""><p>MAKE A RESERVATION &#8594;</p><img src="" /></a>
 		</div>
 	</div>
 </section>
@@ -35,13 +35,14 @@
 			<label>Email Address</label><br />
 			<input type="email" name="email"><br />
 		</form>
-		<button type="submit" form_id="newsletter" value="submit">SUBMIT<img src="" /></button>
+		<button type="submit" form_id="newsletter" value="submit">SUBMIT &#8594;<img src="" /></button>
 	</div>
 	<div class="contact-us">
 		<h2>CONTACT US</h2>
 		<h3>Kevin Smith Transportation Group</h3>
 		<p>123 Main Street</p>
 		<p>Anywhere, PA 12345</p>
+		<br />
 		<p><span class="phone">844.LIMO.KSG</span> (844.546.6574)</p>
 		<p>email@emailaddress.com</p>
 	</div>
