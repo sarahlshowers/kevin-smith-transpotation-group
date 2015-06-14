0d928f6d7a9fc86c14a03b234b6baec09082eefe
sarahlshowers <sarahlshowers@gmail.com>
2015-06-13 20:24:34 -0700

    adding inner footer to wrap for max-width styling for extra large viewports header and footer tested on iPad2 and 13" displays

--- app/wp-content/themes/kstg/footer.php
+++ app/wp-content/themes/kstg/footer.php
@@ -1,42 +1,50 @@
-			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-        <div class="copyright">
-           <nav>
-            <p class="kstg">© 2015 Kevin Smith Transportation Group</p>
-            <ul class="nav">
-              <li>
-               <a href=""><p>Legal</p></a>
-              </li>
-              <li>
-                <a href=""><p>Lorem Ipsum</p></a>
-              </li>
-              <li>
-                <a href=""><p>Lorem Ipsum</p></a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-       <div class="social">
-         <ul>
-           <li>
-              <a href="https://www.facebook.com/kevinsmithgroup" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-facebook.png" alt="Facebook" /></a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com/Kevinsmithgrp" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-twitter.png" alt="Twitter" /></a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/company/kevin-smith-transportation-group" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-linkedin.png" alt="LinkedIn" /></a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/channel/UCMeEufGeiAWaxOsIS0Lwegg" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-youtube.png" alt="YouTube" /></a>
-            </li>
-            <li>
-              <a href="https://plus.google.com/b/115066308991397006542/115066308991397006542/posts" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-gplus.png" alt="Google Plus" /></a>
-            </li>
-         </ul>
-       </div>
-     </footer>
-		</div>
-		<?php // all js scripts are loaded in library/bones.php ?>
-		<?php wp_footer(); ?>
- </body>
- </html> <!-- end of site. what a ride! -->
+<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+  <div class="inner-footer">
+    <div class="copyright">
+      <nav>
+        <p class="kstg">© 2015 Kevin Smith Transportation Group</p>
+        <ul class="nav">
+          <li>
+            <a href="">
+              <p>Legal</p>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <p>Lorem Ipsum</p>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <p>Lorem Ipsum</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="social">
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/kevinsmithgroup" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-facebook.png" alt="Facebook" /></a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/Kevinsmithgrp" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-twitter.png" alt="Twitter" /></a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/company/kevin-smith-transportation-group" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-linkedin.png" alt="LinkedIn" /></a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/channel/UCMeEufGeiAWaxOsIS0Lwegg" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-youtube.png" alt="YouTube" /></a>
+        </li>
+        <li>
+          <a href="https://plus.google.com/b/115066308991397006542/115066308991397006542/posts" target="_blank"><img class="fluid-images" src="<?php bloginfo('template_url'); ?>/library/images/icon-gplus.png" alt="Google Plus" /></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
+</div>
+<?php // all js scripts are loaded in library/bones.php ?>
+<?php wp_footer(); ?>
+</body>
+</html> <!-- end of site. what a ride! -->
