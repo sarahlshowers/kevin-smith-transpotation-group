6d7bffcb2aacc2f7f3ef851ee7cb010d1c1a2b3a
sarahlshowers <sarahlshowers@gmail.com>
2015-07-05 13:26:55 -0700

    js in progress for carousel

--- app/wp-content/themes/kstg/library/js/fleet.js
+++ app/wp-content/themes/kstg/library/js/fleet.js
@@ -1,29 +1,46 @@
 var galleryImages = document.querySelectorAll('.carousel-images ul li');
+//Initialize counter at 0
+var counter = 0;
 
 // if gallery image === [0]
-  //disable button '.next' (37px)
-  //change position of image sprite
+  //disable button
+  //change position of image sprite '.prev' (0)
+//else active button
+  //change position of image sprite .prev (-35px)
 
-//if gallery image === galleryImages.length
-  //disable button '.prev' (-35px)
-  //change position of image sprite
+//if gallery image[3] === galleryImages.length
+  //disable button
+  //change position of image sprite '.next' (37px)
+//else active button
+  //change position of image sprite .prev (70px)
+
+
 
 
 (function($) {
   $(document).ready(function() {
     console.log( "ready!" );
     $('.next').on('click', function() {
+      counter++;
+      changeActiveClassNext();
+      changeButtonState();
       console.log('clicked!');
       $('ul').animate({
         left: '-=210'
       }, 500);
     });
     $('.prev').on('click', function() {
+      counter--;
+      changeActiveClassPrev();
+      changeButtonState();
       console.log('clicked!');
       $('ul').animate({
         left: '+=210'
       }, 500);
     });
   });
+
+  changeActiveClassNext()
+  changeButtonState()
 })(jQuery);
 
