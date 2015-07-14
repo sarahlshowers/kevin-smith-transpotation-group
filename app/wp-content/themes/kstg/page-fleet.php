e383e94008c499d01c15193d8128c831f3a1c947
sarahlshowers <sarahlshowers@gmail.com>
2015-07-13 21:34:51 -0700

    Initial desktop styling for fleet overview Add h4 to typography Some new classed to markup for styling purposes

--- app/wp-content/themes/kstg/page-fleet.php
+++ app/wp-content/themes/kstg/page-fleet.php
@@ -22,49 +22,55 @@
 // if we have subcategories, let's get going!
   if( $childPages ): ?>
 
-
-  <?php
-
-    // first, let's loop through all our subcategories (stretch limos, ...)
-  foreach( $childPages as $post):
-
-    // setup $post as a global to change our context
-    setup_postdata($post);
-
-  ?>
-
-  <!-- any markup for the subcats goes here (e.g. the heading) -->
   <div class="fleet-overview">
-    <h2><?php the_title(); ?></h2>
+    <section>
+          <h2>Our Fleet</h2>
+          <div class="content">
+            <?php
 
-    <?php
+          // first, let's loop through all our subcategories (stretch limos, ...)
+            foreach( $childPages as $post):
 
-        // now, we need the actual vehicles, all pages that are a child of the
-        // current $post... ends up happening for each subcategory
-    $vehiclePages = get_pages(array(
-      'child_of' => get_the_ID()
-      ));
+          // setup $post as a global to change our context
+              setup_postdata($post);
 
-      if( $vehiclePages ): ?>
+            ?>
 
-      <!-- any markup for the vehicles goes here -->
-      <ul>
-        <?php foreach( $vehiclePages as $post): ?>
-        <?php setup_postdata($post); ?>
-        <li>
-          <h4><?php the_field('vehicle_type'); ?></h4>
-          <img src="<?php echo get_field('hero_image')['url']; ?>" height="200" width="500" />
-        </li>
+            <!-- any markup for the subcats goes here (e.g. the heading) -->
+
+
+            <h3><?php the_title(); ?></h3>
+
+            <?php
+
+                // now, we need the actual vehicles, all pages that are a child of the
+                // current $post... ends up happening for each subcategory
+            $vehiclePages = get_pages(array(
+              'child_of' => get_the_ID()
+              ));
+
+              if( $vehiclePages ): ?>
+
+              <!-- any markup for the vehicles goes here -->
+              <ul>
+                <?php foreach( $vehiclePages as $post): ?>
+                <?php setup_postdata($post); ?>
+                <li>
+                  <img src="<?php echo get_field('hero_image')['url']; ?>" height="200" width="500" />
+                  <h4><?php the_field('vehicle_type'); ?> </h4>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+            <!-- end of vehicle area -->
+
+
+        <?php wp_reset_postdata(); //reset the context ?>
+      <?php endif; // end of vehicle area, php style ?>
+      <!-- end of subcat area -->
       <?php endforeach; ?>
-    </ul>
-  </div>
-  <!-- end of vehicle area -->
-
-  <?php wp_reset_postdata(); //reset the context ?>
-<?php endif; // end of vehicle area, php style ?>
-<!-- end of subcat area -->
-<?php endforeach; ?>
-<?php wp_reset_postdata(); ?>
-<?php endif; ?>
-
+      <?php wp_reset_postdata(); ?>
+      <?php endif; ?>
+        </div>
+    </section>
+</div>
 <?php get_footer(); ?>
