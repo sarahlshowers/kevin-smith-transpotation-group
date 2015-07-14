adb050f3f5ef5c585b7ac4a579329da86c79beb6
Derrick Showers <dshowers@linkedin.com>
2015-07-13 21:35:34 -0700

    added wordpressdeploy task and pull_db as grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,14 +27,42 @@ module.exports = function(grunt) {
           spawn: false
         }
       }
+    },
+    wordpressdeploy: {
+      options: {
+        backup_dir: 'backups/',
+        target: 'staging',
+        rsync_args: ['--verbose', '--progress', '-rlpt', '--compress', '--omit-dir-times', '--delete'],
+        exclusions: ['Gruntfile.js', '.git/', 'tmp/*', 'backups/', 'wp-config.php', 'composer.json', 'composer.lock', 'README.md', '.gitignore', 'package.json', 'node_modules']
+      },
+      local: {
+        'title': 'local',
+        'database': 'kstg',
+        'user': 'root',
+        'host': '127.0.0.1',
+        'url': 'http://localhost:9000',
+        'path': '/Users/dshowers/Development/kevin-smith-transpotation-group/app'
+      },
+      staging: {
+        'title': 'staging',
+        'database': 'kstg',
+        'user': 'dev_user',
+        'pass': 'mysql',
+        'host': 'localhost',
+        'url': 'http://kstg.dev.derrickshowers.com',
+        'path': '/var/www/sites/kstg',
+        'ssh_host': 'dshowers@dev.derrickshowers.com'
+      }
     }
   });
 
   // load plugins
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-wordpress-deploy');
 
   // tasks
   grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('update', ['pull_db']);
 
 };
--- README.md
+++ README.md
@@ -15,3 +15,18 @@ This site contains static pages and a Wordpress app. Therefore, the setup that m
 ```
 
 Where `landingPages/` contains landing pages separated into individual directories and `app/` contains all Wordpress resources (with the exception of config files). The root contains anything that would be necessary for local development. In the end, anything within the app directory gets deployed 1:1 to production (same with directories with `landingPages/`).
+
+## Grunt
+
+### Prereqs
+
+Ensure your ssh key is authorized on the dev server (dev.derrickshowers.com). To do so, run the following command (replacing '<user>' with your username).
+
+    cat ~/.ssh/id_rsa.pub | ssh <user>@dev.derrickshowers.com 'cat >> .ssh/authorized_keys'
+
+Make sure you can log onto the server without a password.
+
+    ssh <user>@dev.derrickshowers.com
+
+That's it!
+
