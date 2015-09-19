72dad0c5055a009a83d0df0dfc894d336a3b5e80
sarahlshowers <sarahlshowers@gmail.com>
2015-09-19 15:43:11 -0700

    meta data for seo

--- app/wp-content/themes/kstg/index.php
+++ app/wp-content/themes/kstg/index.php
@@ -5,6 +5,6 @@ Don't think this appears anywhere, unless there's some sort of issue (e.g. a tem
 Placed some dummy text for now so we know what page is being shown. We may want to fiugre out something
 else to do with this page eventually.
 -->
-<h1>Kevin Smith Transportation Group index page</h1>
+<!-- <h1>Kevin Smith Transportation Group index page</h1> -->
 
 <?php get_footer(); ?>
